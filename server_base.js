749307899205b9fa01ffffeb6117c4b0adfe19bb
wiolag123 <kogut.wioletta@gmail.com>
2022-04-09 14:30:45 +0200

    zmiana w porcie

--- server_base.js
+++ server_base.js
@@ -1,9 +1,8 @@
 const http = require('http');
-const port = process.env.PORT;
+const port = process.env.PORT; || 8080
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end('Wiola jest super SUPER!');
 }
 const server = http.createServer(requestListener);
 server.listen(port);
-}
